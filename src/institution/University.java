c1b4cb2b656ba0a01177638e6f73147805987cca
unknown <lytsus.viktor@gmail.com>
2018-08-06 17:51:36 +0300

    University class implementation

--- src/institution/University.java
+++ src/institution/University.java
@@ -1,17 +1,30 @@
 package institution;
 
 import person.Student;
+import person.consciousness.Knowledge;
 
 public class University {
+    String university_name;
+    private Student[] students = new Student[0];
+
     public University(String name) {
-        //TODO: Implementation is needed
+        university_name = name;
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    public void setStudent(String name, int level) {
+        Student student = new Student(name);
+        student.setKnowledge(new Knowledge(level));
+        addStudent(student);
     }
 
-    public void addStudent(Student student) {
-        //TODO: Implementation is needed
+    private void addStudent(Student student) {
+        Student[] tempArr = new Student[students.length+1];
+        System.arraycopy(students, 0, tempArr, 0, students.length);
+        tempArr[students.length] = student;
+        students = tempArr;
+    }
+
+    public Student[] getStudents() {
+        return students;
     }
 }
