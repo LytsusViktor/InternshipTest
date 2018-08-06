f4d0d3d2f7fd10d8c99ddaf384b7522c5be18af5
unknown <lytsus.viktor@gmail.com>
2018-08-06 19:34:34 +0300

    Internship class impementation. Use implemented classes for demonsration in main class.

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -1,18 +1,32 @@
 package institution.interlink;
 
 import person.Student;
+import institution.University;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Internship {
+    String name;
     public Internship(String name) {
-        //TODO: Implementation is needed
+        this.name = name;
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    private double getAverageMark (Student[] students) {
+        double sum = 0;
+        for (int i = 0; i < students.length; i++)
+            sum += students[i].knowledge.getKnowledge();
+        return sum/students.length;
     }
 
-    public String getStudents() {
-        //TODO: Implementation is needed
-        return "Andrew Maslenko\nJulia Veselkina\n";
+    public String getStudents(University university) {
+        Student[] students = university.getStudents();
+        List<String> internshipMembers = new ArrayList<>();
+        double average = getAverageMark(students);
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].knowledge.getKnowledge() > average) {
+                internshipMembers.add(students[i].name);
+            }
+        }
+        return String.join(", ", internshipMembers);
     }
 }
--- src/main/Application.java
+++ src/main/Application.java
@@ -7,12 +7,13 @@ import person.Student;
 public class Application {
     public static void main(String[] args) {
         University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
-
+        university.setStudent("Andrew Kostenko", 4);
+        university.setStudent("Yurii Kostenko", 5);
+        university.setStudent("Bohdan Kostenko", 4);
+        university.setStudent("Julia Veselkina", 3);
+        university.setStudent("Maria Perechrest", 5);
         Internship internship = new Internship("Interlink");
         System.out.println("List of internship's students:");
-        System.out.println(internship.getStudents());
+        System.out.println(internship.getStudents(university));
     }
 }
