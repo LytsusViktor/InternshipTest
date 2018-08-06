93d42faeda5b31933beaad2c84cadd64d1a922d5
unknown <lytsus.viktor@gmail.com>
2018-08-06 17:50:32 +0300

    Add getter in Knowledge class to get level

--- src/person/consciousness/Knowledge.java
+++ src/person/consciousness/Knowledge.java
@@ -1,8 +1,11 @@
 package person.consciousness;
 
 public class Knowledge {
-    public int level;
+    private int level;
     public Knowledge(int level) {
         this.level = level;
+    }
+    public int getKnowledge() {
+        return this.level;
     }
 }
